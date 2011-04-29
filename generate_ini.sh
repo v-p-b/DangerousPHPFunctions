885aea86372434f29624c8d29b9b8c9320227de8
v-p-b <vpbalint@gmail.com>
2011-04-29 18:08:38 +0200

    Revert "ignoring comments"
    This reverts commit f79977990971ec09aa90021adbbae5c6b392343e.

--- generate_ini.sh
+++ generate_ini.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo 'disabled_functions="'`grep -E -v '^\[|^\s*$' $1 | cut -d '#' -f1 | sort | tr '\n' ','| sed 's/,$//'`'"'
+echo 'disabled_functions="'`grep -E -v '^\[|^\s*$' $1 | sort | tr '\n' ','| sed 's/,$//'`'"'
 
